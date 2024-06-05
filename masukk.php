45c6af097a40c8cfbf450421c60f4dab4b236873
anhar-rs <belajargaess@gmail.com>
2024-06-05 13:52:25 +0800

    login.php

---
+++ masukk.php
@@ -0,0 +1,155 @@
+<?php 
+require 'function.php';
+
+if( isset($_POST["daftar"]) ) {
+
+	if( registrasi($_POST) > 0 ) {
+		echo "<script>
+				alert('user baru berhasil ditambahkan!');
+			  </script>";
+	} else {
+		echo mysqli_error($conn);
+	}
+
+}
+
+session_start();
+
+if( isset($_SESSION["masuk"]) ) {
+	header("Location: index.php");
+	exit;
+}
+if( isset($_POST["login"]) ) {
+
+	$username = $_POST["username"];
+	$password = $_POST["password"];
+
+	$result = mysqli_query($conn, "SELECT * FROM users WHERE username = '$username'");
+
+	// cek username
+	if( mysqli_num_rows($result) === 1 ) {
+
+		// cek password
+		$row = mysqli_fetch_assoc($result);
+		if( password_verify($password, $row["password"]) ) {
+			// set session
+			$_SESSION["login"] = true;
+
+			header("Location: index.php");
+			exit;
+		}
+	}
+
+	$error = true;
+
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>SignIn&SignUp</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <script
+      src="https://kit.fontawesome.com/64d58efce2.js"
+      crossorigin="anonymous"
+    ></script>
+  
+  </head>
+  <body>
+    <div class="container">
+      <div class="forms-container">
+        <div class="signin-signup">
+          <form action="dashboard.html" class="sign-in-form" method="POST">
+            <h2 class="title">Sign In</h2>
+            <div class="input-field">
+              <i class="fas fa-user"></i>
+              <input type="text" placeholder="Username"  name="username"/>
+            </div>
+            <div class="input-field">
+              <i class="fas fa-lock"></i>
+              <input type="password" placeholder="Password" name="password" />
+            </div>
+            <button type="submit" name="masuk" class="btn solid">Sign In</button>
+
+            <p class="social-text">Or Sign in with social platforms</p>
+            <div class="social-media">
+              <a href="#" class="social-icon">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+              <a href="#" class="social-icon">
+                <i class="fab fa-twitter"></i>
+              </a>
+              <a href="#" class="social-icon">
+                <i class="fab fa-google"></i>
+              </a>
+              <a href="#" class="social-icon">
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+            </div>
+          </form>
+
+
+          <form action="dashboard.html" class="sign-up-form" method="POST">
+            <h2 class="title">Sign Up</h2>
+            <div class="input-field">
+              <i class="fas fa-user"></i>
+              <input type="text" placeholder="Username" name="username" >
+            </div>
+            <div class="input-field">
+              <i class="fas fa-envelope"></i>
+              <input type="email" placeholder="Email" name="email" >
+            </div>
+            <div class="input-field">
+              <i class="fas fa-lock"></i>
+              <input type="password" placeholder="Password" name="password" >
+            </div>
+              <button type="submit" name="daftar" class="btn solid">Sign Up</button>
+
+            <p class="social-text">Or Sign up with social platforms</p>
+            <div class="social-media">
+              <a href="#" class="social-icon">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+              <a href="#" class="social-icon">
+                <i class="fab fa-twitter"></i>
+              </a>
+              <a href="#" class="social-icon">
+                <i class="fab fa-google"></i>
+              </a>
+              <a href="#" class="social-icon">
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div class="panels-container">
+
+        <div class="panel left-panel">
+            <div class="content">
+                <h3>New here?</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio minus natus est.</p>
+                <button class="btn transparent" id="sign-up-btn">Sign Up</button>
+            </div>
+            <img src="./img/1.png" class="image" alt="">
+        </div>
+
+        <div class="panel right-panel">
+            <div class="content">
+                <h3>One of us?</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio minus natus est.</p>
+                <button class="btn transparent" id="sign-in-btn">Sign In</button>
+            </div>
+            <img src="./img/2.png" class="image" alt="">
+        </div>
+      </div>
+    </div>
+
+    <script src="./app.js"></script>
+  </body>
+</html>
